0370e9118633ad74106bcb6beb9f1d930b3d3287
Yana-Lesina <yanalesina3@gmail.com>
2022-11-19 00:07:30 +0300

    feature: changee X/O players and find winner

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import Square from "../Square/Square";
 
-class Board extends React.Component<{}, { squares: any[] }> {
+class Board extends React.Component<
+  {},
+  { squares: string[]; xIsNext: boolean }
+> {
   constructor(props: Readonly<{}>) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
   handleClick(i: number) {
     const newSquares = this.state.squares.slice();
-    newSquares[i] = "X";
-    this.setState({ squares: newSquares });
+    newSquares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({ squares: newSquares, xIsNext: !this.state.xIsNext });
   }
 
   renderSquare(id: number) {
@@ -23,24 +27,50 @@ class Board extends React.Component<{}, { squares: any[] }> {
     );
   }
 
-  render() {
-    const status = `Next player: ${""}`;
+  calculateWinner(squares: string[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  render() {
+    //const status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    const winner = this.calculateWinner(this.state.squares);
+    const status = !winner
+      ? `Next player: ${this.state.xIsNext ? "X" : "O"}`
+      : `We have winner: ${winner}`;
     return (
-      <div>
-        <>
-          <div className="status">{status}</div>
-          {[0, 1, 2].map((_, id) => {
-            return (
-              <div key={id} className="board-row">
-                {this.renderSquare(id)}
-                {this.renderSquare(id + 3)}
-                {this.renderSquare(id + 3 * 2)}
-              </div>
-            );
-          })}
-        </>
-      </div>
+      <>
+        <div className="status">{status}</div>
+        {[0, 1, 2].map((_, id) => {
+          return (
+            <div key={id} className="board-row">
+              {this.renderSquare(id)}
+              {this.renderSquare(id + 3)}
+              {this.renderSquare(id + 3 * 2)}
+            </div>
+          );
+        })}
+      </>
     );
   }
 }
