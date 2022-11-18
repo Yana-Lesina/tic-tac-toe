a84f0c2f4b413c9941f3fcf66e89fc4ee91f0bed
Yana-Lesina <yanalesina3@gmail.com>
2022-11-18 18:49:40 +0300

    fix: X is now drawn in only one square

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -13,9 +13,9 @@ class Board extends React.Component<{}, { squares: any[] }> {
     };
   }
   handleClick(i: number) {
-    const newSquares = this.state.squares.slice(); // копируем массив
-    newSquares[i] = "X"; // меняем нужное значение
-    this.setState({ squares: newSquares }); // перезаписываем состояние Board
+    const newSquares = this.state.squares.slice();
+    newSquares[i] = "X";
+    this.setState({ squares: newSquares });
   }
 
   renderSquare(id: number) {
@@ -38,9 +38,9 @@ class Board extends React.Component<{}, { squares: any[] }> {
           {[0, 1, 2].map((_, id) => {
             return (
               <div key={id} className="board-row">
-                {this.renderSquare(id)} {/* по ссылке или по значению? */}
-                {this.renderSquare(++id)}
-                {this.renderSquare(++id)}
+                {this.renderSquare(id)}
+                {this.renderSquare(id + 3)}
+                {this.renderSquare(id + 3 * 2)}
               </div>
             );
           })}
@@ -51,8 +51,3 @@ class Board extends React.Component<{}, { squares: any[] }> {
 }
 
 export default Board;
-
-// как оптимизировать рендер циклом? - сделала
-// Что я забыла? Какие были ошибки?
-// использовала forEach вместо .map, поэтому компоненты Square не рендерились. Всспомнила только благодаря гуглу
-// забыла про свойство key для корректного ререндера
